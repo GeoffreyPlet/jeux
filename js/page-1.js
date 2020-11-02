29c91e274929e780cdc000fb7f005b542df39ea5
GeoffreyPlet <geoffreyparle@gmail.com>
2020-11-02 12:32:37 +0100

    DEBUT page 2

--- js/page-1.js
+++ js/page-1.js
@@ -177,7 +177,7 @@ function createDivContent(name, content){
                         }
                     }
                     if(check === true){
-                        console.log("gg");
+                        pageTwo();
                     }
                 });
             /* FIN ENQUETE */
@@ -186,9 +186,52 @@ function createDivContent(name, content){
     }
 /* FIN 1er Page */
 
-pageOne();
-document.body.innerHTML = '';
+
 
 /* DEBUT 2eme PAGE */
-    
+    function pageTwo(){
+        
+        document.body.innerHTML = '';
+        var divUtil
+        var livre = createDivClass('div', 'container border mt-5');
+        var livreRow = createDivClass('div', 'row h-100');
+        var page = [createDivClass('div', 'col-6 h-100 d-flex flex-column justify-content-center text-center'), createDivClass('div', 'col-6 h-100 border-left')];
+
+        /* DEBUT init LIVRE */
+            livre.style.height = '800px';
+
+            document.body.appendChild(livre);
+            document.getElementsByClassName('container')[0].appendChild(livreRow);
+            document.getElementsByClassName('row')[0].appendChild(page[0]);
+            document.getElementsByClassName('row')[0].appendChild(page[1]);
+        /* FIN init LIVRE */
+
+        /* DEBUT init PAGE LEFT */
+            var pageLeft = document.getElementsByClassName('col-6')[0];
+            
+            pageLeft.appendChild(createDivContent('h2', 'Bienvenu voyageur'));
+            divUtil = createDivContent('p', 'Serriez-vous à la hauteur ?')
+            pageLeft.appendChild(divUtil);
+            divUtil = createDivClass('p', 'position-absolute bot right m-0');
+            divUtil.textContent = 'page 3';
+            pageLeft.appendChild(divUtil);
+
+        /* FIN init PAGE LEFT */
+
+        /* DEBUT init PAGE RIGHT */
+            var pageRight = document.getElementsByClassName('col-6')[1];
+            pageRight.appendChild(createDivClass('div', 'container h-100 position-relative'));
+            pageRight.getElementsByClassName('container')[0].appendChild(createDivClass('div', 'row text-center w-100 h-75 position-absolute top-10'));
+
+            /* DEBUT TITRE */
+                divUtil = createDivContent('h3', 'Trouvez le code');
+                divUtil.style.width = '100%';
+                pageRight.getElementsByClassName('row')[0].appendChild(divUtil);
+            /* FIN TITRE */
+
+        /* FIN init PAGE RIGHT */
+    }
 /* FIN 2eme PAGE */
+
+pageOne();
+
